31944439efe2fbdc744b1d24a179db69f4ebc143
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-07-23 14:24:52 +0100

    filter updated

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -62,7 +62,6 @@ function Filter() {
   };
 
   const cityName = searchParams.get('city');
-  const resultCount = searchParams.toString() ? 'resultados' : 'propriedades';
 
   return (
     <div className='filter'>
@@ -76,11 +75,6 @@ function Filter() {
             'Pesquisar Propriedades'
           )}
         </h1>
-        {searchParams.toString() && (
-          <button className='clearButton' onClick={handleClearFilters}>
-            Limpar filtros
-          </button>
-        )}
       </div>
 
       <div className='filterForm'>
@@ -182,9 +176,37 @@ function Filter() {
 
           <div className='buttonGroup'>
             <button className='searchButton' onClick={handleFilter}>
-              <img src='/search.png' alt='Pesquisar' />
+              <svg
+                width='18'
+                height='18'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+              >
+                <circle cx='11' cy='11' r='8' />
+                <path d='M21 21l-4.35-4.35' />
+              </svg>
               <span>Pesquisar</span>
             </button>
+
+            {searchParams.toString() && (
+              <button className='clearButton' onClick={handleClearFilters}>
+                <svg
+                  width='16'
+                  height='16'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                >
+                  <path d='M3 6h18' />
+                  <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+                  <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+                </svg>
+                Limpar
+              </button>
+            )}
           </div>
         </div>
       </div>
